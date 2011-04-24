8e5666a893d70454d802bdc1fc6d6c12e91afc35
sparky <sparky@pld-linux.org>
2011-04-24 22:35:15 +0200

    - some more stuff

--- lib/WWW/CurlOO/Simple.pm
+++ lib/WWW/CurlOO/Simple.pm
@@ -3,7 +3,8 @@ package WWW::CurlOO::Simple;
 use strict;
 use warnings;
 use WWW::CurlOO qw(/^CURL_VERSION_/);
-use WWW::CurlOO::Easy qw(/^CURLOPT_/);
+use WWW::CurlOO::Easy qw(/^CURLOPT_/ /^CURLPROXY_/);
+use Scalar::Util qw(looks_like_number);
 use base qw(WWW::CurlOO::Easy);
 
 our $VERSION = '0.01';
@@ -11,15 +12,44 @@ our $VERSION = '0.01';
 my @common_options = (
 	CURLOPT_TIMEOUT, 300,
 	CURLOPT_CONNECTTIMEOUT, 60,
-	CURLOPT_MAXREDIRS, 20,
 	CURLOPT_FOLLOWLOCATION, 1,
+	CURLOPT_MAXREDIRS, 50,
 	CURLOPT_SSL_VERIFYPEER, 0,
+	CURLOPT_NOPROGRESS, 1,
 	CURLOPT_COOKIEFILE, '',
 	CURLOPT_USERAGENT, 'WWW::CurlOO::Simple',
+	CURLOPT_HEADERFUNCTION, \&_cb_header,
+	CURLOPT_HTTPHEADER, [
+		'Accept: */*',
+	],
+);
+
+my %proxy = (
+	http    => CURLPROXY_HTTP,
+	http10  => CURLPROXY_HTTP_1_0,
+	socks4  => CURLPROXY_SOCKS4,
+	socks4a => CURLPROXY_SOCKS4A,
+	socks5  => CURLPROXY_SOCKS5,
+	socks   => CURLPROXY_SOCKS5,
+	socks5host => CURLPROXY_SOCKS5_HOSTNAME,
 );
 
 if ( WWW::CurlOO::version_info()->{features} & CURL_VERSION_LIBZ ) {
 	push @common_options, CURLOPT_ENCODING, 'gzip,deflate';
+}
+
+my %option2curlopt = map { lc $_ => eval "WWW::CurlOO::Easy::CURLOPT_\U$_" }
+	qw(timeout useragent proxy interface url referer range httpheader);
+
+sub setopt
+{
+	my ( $easy, $opt, $val ) = @_;
+	unless ( looks_like_number( $opt ) ) {
+		die "unrecognized literal option: $opt\n"
+			unless exists $option2curlopt{ lc $opt };
+		$opt = $option2curlopt{ lc $opt };
+	}
+	return $easy->SUPER::setopt( $opt => $val );
 }
 
 sub setopts
@@ -31,13 +61,20 @@ sub setopts
 	}
 }
 
+sub _cb_header
+{
+	my ( $easy, $data, $uservar ) = @_;
+	push @{ $easy->{headers} }, $data;
+	return length $data;
+}
+
 sub new
 {
 	my $class = shift;
 	my $uri = shift;
 
 	my $easy = $class->SUPER::new(
-		{ body => '', headers => '' }
+		{ body => '', headers => [] }
 	);
 	# some sane defaults
 	$easy->setopts(
@@ -79,8 +116,8 @@ sub _perform
 	$easy->{body} = '';
 	$easy->{headers} = [];
 	$easy->{in_use} = 1;
-	if ( exists $INC{"WWW::CurlOO::Async"} ) {
-		WWW::CurlOO::Async::add( $easy );
+	if ( exists $INC{"WWW::CurlOO::Simple::Async"} ) {
+		WWW::CurlOO::Simple::Async::add( $easy );
 	} else {
 		eval {
 			$easy->perform();
@@ -94,27 +131,31 @@ sub _perform
 sub get
 {
 	my ( $easy, $uri, $cb ) = @_;
-	$easy->setopt( CURLOPT_HTTPGET, 1 );
-	$easy->_perform( $uri, $cb );
+	$easy->_perform( $uri, $cb,
+		CURLOPT_HTTPGET, 1,
+	);
 }
 
 # request head on some uri
 sub head
 {
 	my ( $easy, $uri, $cb ) = @_;
-	$easy->setopt( CURLOPT_NOBODY, 1 );
-	$easy->_perform( $uri, $cb );
+	$easy->_perform( $uri, $cb,
+		CURLOPT_NOBODY, 1,
+	);
 }
 
 # post data to some uri
 sub post
 {
 	my ( $easy, $uri, $cb, $post ) = @_;
-	$easy->setopt( CURLOPT_POST, 1 );
-	$easy->setopt( CURLOPT_POSTFIELDS, $post );
-	$easy->setopt( CURLOPT_POSTFIELDSIZE, length $post );
-	$easy->_perform( $uri, $cb );
+	$easy->_perform( $uri, $cb,
+		CURLOPT_POST, 1,
+		CURLOPT_POSTFIELDS, $post,
+		CURLOPT_POSTFIELDSIZE, length $post,
+	);
 }
+
 
 1;
 
