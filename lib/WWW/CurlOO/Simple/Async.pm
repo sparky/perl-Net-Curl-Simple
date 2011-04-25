e0a54a8e1f2a7d634e9a3afe36d2933039263f44
sparky <sparky@pld-linux.org>
2011-04-25 04:04:10 +0200

    - ok, this part of Async execution is mostly done

--- lib/WWW/CurlOO/Simple/Async.pm
+++ lib/WWW/CurlOO/Simple/Async.pm
@@ -3,6 +3,48 @@ package WWW::CurlOO::Simple::Async;
 use strict;
 use warnings;
 
+my @backends = (
+	AnyEvent => 'AnyEvent',
+	# POE => 'POE::Kernel',
+	# POE => 'Wx',
+	# POE => 'Prima',
+	# IO_Async => 'IO::Async::Loop',
+	# EV => 'EV',
+	# Glib => 'Glib',
+	Irssi => 'Irssi',
+	Perl => undef, # direct approach
+);
+
+my $multi;
+sub _make_multi
+{
+	no strict 'refs';
+	while ( my ( $impl, $pkg ) = splice @backends, 0, 2 ) {
+		if ( not defined $pkg or defined ${ $pkg . '::VERSION' } ) {
+			my $implpkg = join '::', __PACKAGE__, $impl;
+			eval "require $implpkg";
+			eval '$multi = $implpkg->new()';
+			last if $multi;
+		}
+	}
+	@backends = ();
+	die "Could not load WWW::CurlOO::Simple::Async implementation\n"
+		unless $multi;
+}
+
+sub add
+{
+	my $easy = shift;
+	_make_multi() unless $multi;
+	$multi->add_handle( $easy );
+}
+
+sub loop
+{
+	_make_multi() unless $multi;
+	$multi->loop();
+}
+
 1;
 
 =head1 NAME
