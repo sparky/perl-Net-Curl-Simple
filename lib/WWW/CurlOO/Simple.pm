64b67e4061b4a0b670c4d685cd551d7a3c527e53
sparky <sparky@pld-linux.org>
2011-04-25 02:45:14 +0200

    - simplified options

--- lib/WWW/CurlOO/Simple.pm
+++ lib/WWW/CurlOO/Simple.pm
@@ -12,16 +12,16 @@ use base qw(WWW::CurlOO::Easy);
 our $VERSION = '0.01';
 
 my @common_options = (
-	CURLOPT_TIMEOUT, 300,
-	CURLOPT_CONNECTTIMEOUT, 60,
-	CURLOPT_FOLLOWLOCATION, 1,
-	CURLOPT_MAXREDIRS, 50,
-	CURLOPT_SSL_VERIFYPEER, 0,
-	CURLOPT_NOPROGRESS, 1,
-	CURLOPT_COOKIEFILE, '',
-	CURLOPT_USERAGENT, "WWW::CurlOO::Simple v$VERSION",
-	CURLOPT_HEADERFUNCTION, \&_cb_header,
-	CURLOPT_HTTPHEADER, [
+	timeout => 300,
+	connecttimeout => 60,
+	followlocation => 1,
+	maxredirs => 50,
+	ssl_verifypeer => 0,
+	noprogress => 1,
+	cookiefile => '',
+	useragent => "WWW::CurlOO::Simple v$VERSION",
+	headerfunction => \&_cb_header,
+	httpheader => [
 		'Accept: */*',
 	],
 );
@@ -40,35 +40,39 @@ my %proxytype = (
 }
 
 if ( WWW::CurlOO::version_info()->{features} & CURL_VERSION_LIBZ ) {
-	push @common_options, CURLOPT_ENCODING, 'gzip,deflate';
+	push @common_options, encoding => 'gzip,deflate';
 }
 
-my %option2curlopt = map { lc $_ => eval "WWW::CurlOO::Easy::CURLOPT_\U$_" }
-	qw(verbose timeout useragent proxy interface url referer range httpheader);
-
-sub setopt
 {
-	my ( $easy, $opt, $val ) = @_;
+	my %optcache;
 
-	unless ( looks_like_number( $opt ) ) {
-		# convert option name to option number
-		die "unrecognized literal option: $opt\n"
-			unless exists $option2curlopt{ lc $opt };
-		$opt = $option2curlopt{ lc $opt };
+	sub setopt
+	{
+		my ( $easy, $opt, $val ) = @_;
+
+		unless ( looks_like_number( $opt ) ) {
+			# convert option name to option number
+			unless ( exists $optcache{ $opt } ) {
+				eval "\$optcache{ \$opt } = WWW::CurlOO::Easy::CURLOPT_\U$opt";
+				die "unrecognized literal option: $opt\n"
+					if $@;
+			}
+			$opt = $optcache{ $opt };
+		}
+
+		if ( $opt == CURLOPT_PROXY ) {
+			# guess proxy type from proxy string
+			my $type = ( $val =~ m#^([a-z0-9]+)://# );
+			die "unknown proxy type $type\n"
+				unless exists $proxytype{ $type };
+			$easy->SUPER::setopt( CURLOPT_PROXYTYPE, $proxytype{ $type } );
+		} elsif ( $opt == CURLOPT_POSTFIELDS ) {
+			# perl knows the size, but libcurl may be wrong
+			$easy->SUPER::setopt( CURLOPT_POSTFIELDSIZE, length $val );
+		}
+
+		$easy->SUPER::setopt( $opt => $val );
 	}
-
-	if ( $opt == CURLOPT_PROXY ) {
-		# guess proxy type from proxy string
-		my $type = ( $val =~ m#^([a-z0-9]+)://# );
-		die "unknown proxy type $type\n"
-			unless exists $proxytype{ $type };
-		$easy->SUPER::setopt( CURLOPT_PROXYTYPE, $proxytype{ $type } );
-	} elsif ( $opt == CURLOPT_POSTFIELDS ) {
-		# perl knows the size, but libcurl may be wrong
-		$easy->SUPER::setopt( CURLOPT_POSTFIELDSIZE, length $val );
-	}
-
-	$easy->SUPER::setopt( $opt => $val );
 }
 
 sub setopts
@@ -77,6 +81,27 @@ sub setopts
 
 	while ( my ( $opt, $val ) = splice @_, 0, 2 ) {
 		$easy->setopt( $opt => $val );
+	}
+}
+
+{
+	my %infocache;
+
+	sub getinfo
+	{
+		my ( $easy, $info ) = @_;
+
+		unless ( looks_like_number( $info ) ) {
+			# convert option name to option number
+			unless ( exists $infocache{ $info } ) {
+				eval "\$infocache{ \$info } = WWW::CurlOO::Easy::CURLINFO_\U$info";
+				die "unrecognized literal info: $info\n"
+					if $@;
+			}
+			$info = $infocache{ $info };
+		}
+
+		$easy->SUPER::getinfo( $info );
 	}
 }
 
@@ -96,8 +121,8 @@ sub new
 	);
 	# some sane defaults
 	$easy->setopts(
-		CURLOPT_WRITEHEADER, \$easy->{headers},
-		CURLOPT_FILE, \$easy->{body},
+		writeheader => \$easy->{headers},
+		file => \$easy->{body},
 		@common_options,
 		@_,
 	);
@@ -108,9 +133,7 @@ sub new
 sub finish
 {
 	my ( $easy, $result ) = @_;
-	$easy->{referer} = $easy->getinfo(
-		WWW::CurlOO::Easy::CURLINFO_EFFECTIVE_URL
-	);
+	$easy->{referer} = $easy->getinfo( 'effective_url' );
 	$easy->{in_use} = 0;
 
 	my $cb = $easy->{cb};
@@ -124,12 +147,12 @@ sub _perform
 		die "this handle is already in use\n";
 	}
 	if ( $easy->{referer} ) {
-		$easy->setopt( CURLOPT_REFERER, $easy->{referer} );
+		$easy->setopt( referer => $easy->{referer} );
 		$uri = URI->new( $uri )->abs( $easy->{referer} )->as_string;
 	}
 	$easy->setopts(
 		@_,
-		CURLOPT_URL, $uri,
+		url => $uri,
 	);
 	$easy->{uri} = $uri;
 	$easy->{cb} = $cb;
@@ -153,7 +176,7 @@ sub get
 {
 	my ( $easy, $uri, $cb ) = @_;
 	$easy->_perform( $uri, $cb,
-		CURLOPT_HTTPGET, 1,
+		httpget => 1,
 	);
 }
 
@@ -162,7 +185,7 @@ sub head
 {
 	my ( $easy, $uri, $cb ) = @_;
 	$easy->_perform( $uri, $cb,
-		CURLOPT_NOBODY, 1,
+		nobody => 1,
 	);
 }
 
@@ -172,19 +195,19 @@ sub post
 	my ( $easy, $uri, $cb, $post ) = @_;
 	my @postopts;
 	if ( not ref $post ) {
-		@postopts = ( CURLOPT_POSTFIELDS, $post );
+		@postopts = ( postfields => $post );
 	} elsif ( UNIVERSAL::isa( $post, 'WWW::CurlOO::Form' ) ) {
-		@postopts = ( CURLOPT_HTTPPOST, $post );
+		@postopts = ( httppost => $post );
 	} elsif ( ref $post eq 'HASH' ) {
 		# handle utf8 ?
 		my $postdata = join '&',
 			map { uri_escape( $_ ) . '=' . uri_escape( $post->{ $_ } ) }
 			sort keys %$post;
-		@postopts = ( CURLOPT_POSTFIELDS, $postdata );
+		@postopts = ( postfields => $postdata );
 	} else {
 		die "don't know how to convert $post into a valid post\n";
 	}
-	$easy->_perform( $uri, $cb, CURLOPT_POST, 1, @postopts );
+	$easy->_perform( $uri, $cb, post => 1, @postopts );
 }
 
 
