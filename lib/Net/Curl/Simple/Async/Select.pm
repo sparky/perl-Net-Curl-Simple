675d25a59d3c0edcf041793d4b2c549a91579b90
sparky <sparky@pld-linux.org>
2011-05-03 03:00:57 +0200

    - updated perl/select implementation

--- lib/Net/Curl/Simple/Async/Select.pm
+++ lib/Net/Curl/Simple/Async/Select.pm
@@ -1,18 +1,32 @@
 package Net::Curl::Simple::Async::Select;
 
-die 'unfinished';
-
 use strict;
 use warnings;
 use Net::Curl::Multi;
 use base qw(Net::Curl::Multi);
 
-my $loop_run = 0;
-
-sub loop($)
+sub _rip_child($$)
 {
-	my $multi = shift;
-	$loop_run = 1;
+	my ( $multi, $needle ) = @_;
+
+	while ( my ( $msg, $easy, $result ) = $multi->info_read() ) {
+		if ( $msg == Net::Curl::Multi::CURLMSG_DONE ) {
+			$multi->remove_handle( $easy );
+			$easy->_finish( $result );
+
+			if ( not $needle or $needle == $easy ) {
+				return $easy;
+			}
+		} else {
+			die "I don't know what to do with message $msg.\n";
+		}
+	}
+	return undef;
+}
+
+sub _loop($$)
+{
+	my ( $multi, $needle ) = @_;
 
 	my $active = $multi->handles;
 
@@ -27,20 +41,25 @@ sub loop($)
 
 		my $ret = $multi->perform();
 		if ( $active != $ret ) {
-			while ( my ( $msg, $easy, $result ) = $multi->info_read() ) {
-				if ( $msg == Net::Curl::Multi::CURLMSG_DONE ) {
-					$multi->remove_handle( $easy );
-					$easy->_finish( $result );
-				} else {
-					die "I don't know what to do with message $msg.\n";
-				}
-			}
+			$ret = _rip_child( $multi, $needle );
+			return $ret if $ret;
 			$active = $multi->handles;
 		}
 	};
 
 	return;
 }
+
+sub get_one
+{
+	my ( $multi, $easy ) = @_;
+
+	my $ret = _rip_child( $multi, $easy );
+	return $ret if $ret;
+
+	return _loop( $multi, $easy );
+}
+
 
 1;
 
